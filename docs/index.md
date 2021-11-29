99fb52b5fb1539efd0730eac582e1f940f89fc83
valentin.hippolyte <valenthippo@gmail.com>
2021-11-29 11:55:42 +0100

    add cousocus project

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+#PROJET
+
+salut
---
+++ index.php
@@ -0,0 +1,18 @@
+<form action="" method="POST">
+    <label for="name">Entrer une adresse a chercher :</label>
+    <input type="text" name="adresse" id="adresse" required> 
+    <input type="submit" value="Trouver adresse">
+
+
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+if (isset($_POST['adresse'])){
+    $adresse = $_POST['adresse'];
+    $client = new GuzzleHttp\Client();
+    $res = $client->request('GET', 'https://api-adresse.data.gouv.fr/search/?q='.$adresse, [
+        'auth' => ['user', 'pass']
+    ]);
+    $resultat = json_decode($res->getBody(), true);
+    echo "<pre>" .var_dump($resultat) . "</pre>";
+}
